d6786a858ddb91ceb647ba00d93aa1846bc8be44
moon <88218891+man-moon@users.noreply.github.com>
2025-10-22 09:51:03 +0900

    test(view): add move count input test

--- src/test/java/racingcar/view/InputTest.java
+++ src/test/java/racingcar/view/InputTest.java
@@ -2,9 +2,11 @@ package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
 
@@ -12,8 +14,13 @@ class InputTest extends NsTest {
 
     private final InputView inputView = new InputViewImpl();
 
+    @AfterEach
+    void after() {
+        Console.close();
+    }
+
     @Test
-    void 자동차_이름_입력_테스트() {
+    void 자동차_이름_입력() {
         String input = "pobi,woni";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
@@ -21,6 +28,17 @@ class InputTest extends NsTest {
         String carNamesInput = inputView.readCarNames();
 
         assertThat(carNamesInput).isEqualTo(input);
+    }
+
+    @Test
+    void 이동_횟수_입력() {
+        String input = "12";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        String moveCountInput = inputView.readMoveCount();
+
+        assertThat(moveCountInput).isEqualTo(input);
     }
 
     @Override
--- src/test/java/racingcar/view/OutputTest.java
+++ src/test/java/racingcar/view/OutputTest.java
@@ -8,14 +8,21 @@ import org.junit.jupiter.api.Test;
 
 public class OutputTest extends NsTest {
 
-    private final String carNameInputPrompt = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final OutputView outputView = new OutputViewImpl();
 
     @Test
-    void 자동차_이름_입력_안내_메시지_출력_테스트() {
+    void 자동차_이름_입력_안내_메시지_출력() {
         assertSimpleTest(() -> {
-            outputView.requestInputCarNames();
-            assertThat(output()).isEqualTo(carNameInputPrompt);
+            outputView.requestCarNamesInput();
+            assertThat(output()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        });
+    }
+
+    @Test
+    void 이동_횟수_입력_안내_메시지_출력() {
+        assertSimpleTest(() -> {
+            outputView.requestMoveCountInput();
+            assertThat(output()).isEqualTo("시도할 횟수는 몇 회인가요?");
         });
     }
 
